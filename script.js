27ee86bab434e58a1e69a193b8fcbc01054cf9e0
LucasSerafimx <lucas.serafimx@gmail.com>
2023-11-16 22:13:30 -0300

    Desafio Concluido

---
+++ script.js
@@ -0,0 +1,29 @@
+var numeroSecreto = parseInt(Math.random() * 1001);
+var chute;
+var tentativas = 0;
+
+iniciarJogo();
+
+function iniciarJogo(chute) {
+  while (chute !== numeroSecreto) {
+    chute = prompt("Digite um numero entre 1 e 1000");
+    tentativas++;
+
+    if (chute === null) {
+      alert("Você saiu do jogo");
+      return;
+    } else if (tentativas == 20) {
+      alert("Você atingiu o maximo de tentativas, tente novamente");
+      return;
+    } else if (chute == numeroSecreto) {
+      alert("Acertouuu!! o numero é: " + chute);
+      numeroSecreto = parseInt(Math.random() * 1001);
+      return;
+    } else if (chute > numeroSecreto) {
+      alert("Errou... o numero secreto é menor que: " + chute);
+    } else if (chute < numeroSecreto) {
+      alert("Errou... o numero secreto é maior que: " + chute);
+    }
+  }
+}
+alert("Você acertou em " + tentativas + " tentativas!");
